f8f28d059b877ebce29100e2eb76687d789a9c57
sathwik_karne <karnesathwik@gmail.com>
2023-07-18 00:45:00 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import './app.css';
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <div className="container">
+        <Sidebar/>
+        <div className="others">
+          others
+        </div>
+      </div>
     </div>
   );
 }
