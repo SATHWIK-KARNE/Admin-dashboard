6b630d89672f5fe6b2a5c9b186402067db7a4c2b
sathwik_karne <karnesathwik@gmail.com>
2023-07-21 17:56:35 +0530

    users page

---
+++ src/pages/user/User.jsx
@@ -0,0 +1,44 @@
+import "./user.css";
+import { Link } from "react-router-dom";
+import {
+  CalendarToday,
+  LocationSearching,
+  MailOutline,
+  PermIdentity,
+  PhoneAndroid,
+  Publish,
+} from "@mui/icons-material";
+
+const User = () => {
+  return (
+    <div className="user">
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <Link to="/newUser">
+          <button className="userAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="userContainer">
+        <div className="userShow">
+          <div className="userShowTop">
+            <img
+              src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="userShowImg"
+            />
+            <div className="userShowTopTitle">
+              <span className="userShowUsername">Anna Becker</span>
+              <span className="userShowUserTitle">Software Engineer</span>
+            </div>
+          </div>
+          <div className="userShowBottom">
+            
+          </div>
+        </div>
+        <div className="userUpdate"></div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
